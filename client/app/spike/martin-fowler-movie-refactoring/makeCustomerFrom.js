217fbcf60e0abf700355b324fa24d51aebf6306c
WIL PANNELL <wpannell@gmail.com>
2016-04-29 14:49:53 -0700

    rename, extract method and move method refactors

--- client/app/spike/martin-fowler-movie-refactoring/makeCustomerFrom.js
+++ client/app/spike/martin-fowler-movie-refactoring/makeCustomerFrom.js
@@ -1,6 +1,7 @@
-import {REGULAR, NEW_RELEASE, CHILDRENS} from './movie-codes';
+import {NEW_RELEASE} from './movie-codes';
 
 let makeCustomerFrom = (name) => {
+
   let rentals = [];
 
   return {
@@ -13,47 +14,20 @@ let makeCustomerFrom = (name) => {
       let statement = 'Rental Record for ' + name + '\n';
 
       for (let i = 0; i < rentals.length; i++) {
-        let subTotal = 0;
-        let rental = rentals[i];
-
-        //noinspection Eslint
-        switch (rental.movie.priceCode) {
-          case REGULAR:
-
-            subTotal += 2;
-            if (rental.daysRented > 2) {
-              subTotal += (rental.daysRented - 2) * 1.5;
-            }
-            break;
-
-          case NEW_RELEASE:
-
-            subTotal += rental.daysRented * 3;
-            break;
-
-          case CHILDRENS:
-
-            subTotal += 1.5;
-            if (rental.daysRented > 3) {
-              subTotal += (rental.daysRented - 3) * 1.5;
-            }
-
-            break;
-          default:
-
-        }
+        let each = rentals[i];
 
         // add frequent renter points
         frequentRenterPoints++;
 
         // add bonus for a two day new release rental
-        if ((rental.movie.priceCode === NEW_RELEASE) &&
-            rental.daysRented > 1) frequentRenterPoints++;
+        if ((each.movie.priceCode === NEW_RELEASE) &&
+            each.daysRented > 1) frequentRenterPoints++;
 
         //show figures for this rental
-        statement += '\t' + rental.movie.title + '\t' + subTotal.toString(10) + '\n';
+        statement += '\t' + each.movie.title + '\t' +
+            each.getCharge().toString(10) + '\n';
 
-        total += subTotal;
+        total += each.getCharge();
       }
 
       //add footer lines
--- client/app/spike/martin-fowler-movie-refactoring/makeRentalFrom.js
+++ client/app/spike/martin-fowler-movie-refactoring/makeRentalFrom.js
@@ -1,6 +1,40 @@
+import {REGULAR, NEW_RELEASE, CHILDRENS} from './movie-codes';
+
 let makeRentalFrom = (movie, daysRented) => {
+  function getCharge() {
+    let thisAmount = 0;
+    //noinspection Eslint
+    switch (movie.priceCode) {
+      case REGULAR:
+
+        thisAmount += 2;
+        if (daysRented > 2) {
+          thisAmount += (daysRented - 2) * 1.5;
+        }
+        break;
+
+      case NEW_RELEASE:
+
+        thisAmount += daysRented * 3;
+        break;
+
+      case CHILDRENS:
+
+        thisAmount += 1.5;
+        if (daysRented > 3) {
+          thisAmount += (daysRented - 3) * 1.5;
+        }
+
+        break;
+      default:
+
+    }
+    return thisAmount;
+  }
+
 
   return {
+    getCharge: getCharge,
     get movie() { return movie; },
     get daysRented() { return daysRented; }
   };
