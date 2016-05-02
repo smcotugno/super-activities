68d633e9c2d7f7a3a7e4b508ba65486ffa782e48
WIL PANNELL <wpannell@gmail.com>
2016-05-02 13:56:23 -0700

    add negative test to force production code

--- client/app/spike/03-happy-sad-exception-path/happy-sad-exception-path.spec.js
+++ client/app/spike/03-happy-sad-exception-path/happy-sad-exception-path.spec.js
@@ -1,12 +1,13 @@
 import {describe, expect, it} from '../test-helper';
 
-function isPalindrome() {
-   return true;
+function isPalindrome(phrase) {
+  return phrase === phrase.split('').reverse().join('');
 }
 describe('palindrome should', () => {
   it('prove the test infrastructure works', () => {
     expect(true).to.equal(true);
   });
+
   it('madam === true', () => {
     // arrange ...
     const phrase = 'madam';
@@ -16,5 +17,16 @@ describe('palindrome should', () => {
 
     // assert ...
     expect(result).to.equal(true);
+  });
+
+  it('dude === false', () => {
+    // arrange ...
+    const phrase = 'dude';
+
+    // act ...
+    let result = isPalindrome(phrase);
+
+    // assert ...
+    expect(result).to.equal(false);
   });
 });
