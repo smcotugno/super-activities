e287efe17123fc438c79ab51758438b03b0af9a8
WIL PANNELL <wpannell@gmail.com>
2016-05-02 11:48:23 -0700

    introducing specs for palindrome

---
+++ client/app/spike/03-happy-sad-exception-path/happy-sad-exception-path.spec.js
@@ -0,0 +1,7 @@
+import {describe, expect, it} from '../test-helper';
+
+describe('palindrome should', () => {
+  it('prove the test infrastructure works', () => {
+    expect(true).to.equal(true);
+  });
+});
