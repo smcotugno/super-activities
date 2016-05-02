b11ba9880bcd33e0d634dd398d5eda02f151732c
WIL PANNELL <wpannell@gmail.com>
2016-05-02 10:02:13 -0700

    introduce canary test

---
+++ client/app/spike/01-canary-test/canary-test.spec.js
@@ -0,0 +1,7 @@
+import {describe, expect, it} from '../test-helper';
+
+describe('canary test', () => {
+  it('proves the test infrastructure works', () => {
+     expect(true).to.equal(true);
+  });
+});
