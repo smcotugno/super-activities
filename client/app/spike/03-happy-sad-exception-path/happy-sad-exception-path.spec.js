55065d58312a4880998750dd3722fd702fe08191
WIL PANNELL <wpannell@gmail.com>
2016-05-02 12:02:44 -0700

    make kernel test bar green

--- client/app/spike/03-happy-sad-exception-path/happy-sad-exception-path.spec.js
+++ client/app/spike/03-happy-sad-exception-path/happy-sad-exception-path.spec.js
@@ -1,7 +1,20 @@
 import {describe, expect, it} from '../test-helper';
 
+function isPalindrome() {
+   return true;
+}
 describe('palindrome should', () => {
   it('prove the test infrastructure works', () => {
     expect(true).to.equal(true);
+  });
+  it('madam === true', () => {
+    // arrange ...
+    const phrase = 'madam';
+
+    // act ...
+    let result = isPalindrome(phrase);
+
+    // assert ...
+    expect(result).to.equal(true);
   });
 });
