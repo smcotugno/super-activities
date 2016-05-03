23f76b37c3483a9307e4a51ed7d62fb2bc008fe1
WIL PANNELL <wpannell@gmail.com>
2016-05-03 13:15:18 -0700

    redo palindrome

--- client/app/spike/03-happy-sad-exception-path/happy-sad-exception-path.spec.js
+++ client/app/spike/03-happy-sad-exception-path/happy-sad-exception-path.spec.js
@@ -2,130 +2,45 @@ import {describe, expect, it} from '../test-helper';
 
 function isPalindrome(phrase) {
   if (arguments.length === 0) throw new Error('Invalid Argument');
-  if (typeof phrase !== 'string') return false;
-  return (phrase.trim().length > 0) &&
-      phrase === phrase.split('').reverse().join('');
+
+  return (typeof phrase === 'string') &&
+      (phrase.trim().length > 0) &&
+      (phrase === phrase.split('').reverse().join(''));
 }
+
 describe('palindrome should', () => {
   it('prove the test infrastructure works', () => {
     expect(true).to.equal(true);
   });
 
-  it('madam === true', () => {
-    // arrange ...
-    const phrase = 'madam';
-
-    // act ...
-    let result = isPalindrome(phrase);
-
-    // assert ...
-    expect(result).to.equal(true);
+  it('show madam === true', () => {
+    expect(isPalindrome('madam')).to.equal(true);
   });
 
-  it('dude === false', () => {
-    // arrange ...
-    const phrase = 'dude';
-
-    // act ...
-    let result = isPalindrome(phrase);
-
-    // assert ...
-    expect(result).to.equal(false);
+  it('show dude === false', () => {
+    expect(isPalindrome('dude')).to.equal(false);
   });
 
-  it('level === true', () => {
-    // arrange ...
-    const phrase = 'level';
-
-    // act ...
-    let result = isPalindrome(phrase);
-
-    // assert ...
-    expect(result).to.equal(true);
+  it('show mom dad === false', () => {
+    expect(isPalindrome('mom dad')).to.equal(false);
   });
 
-  it('civic === true', () => {
-    // arrange ...
-    const phrase = 'civic';
-
-    // act ...
-    let result = isPalindrome(phrase);
-
-    // assert ...
-    expect(result).to.equal(true);
+  it('show "" === false', () => {
+    expect(isPalindrome('')).to.equal(false);
   });
 
-  it('civic === true', () => {
-    // arrange ...
-    const phrase = 'civic';
-
-    // act ...
-    let result = isPalindrome(phrase);
-
-    // assert ...
-    expect(result).to.equal(true);
+  it('show "    " === false', () => {
+    expect(isPalindrome('    ')).to.equal(false);
   });
 
-  it('mom mom === true', () => {
-    // arrange ...
-    const phrase = 'mom mom';
-
-    // act ...
-    let result = isPalindrome(phrase);
-
-    // assert ...
-    expect(result).to.equal(true);
+  it('show 0 === false', () => {
+    expect(isPalindrome(0)).to.equal(false);
   });
 
-  it('mom dad === false', () => {
-    // arrange ...
-    const phrase = 'mom dad';
-
-    // act ...
-    let result = isPalindrome(phrase);
-
-    // assert ...
-    expect(result).to.equal(false);
-  });
-
-  it(' \'\' === false', () => {
-    // arrange ...
-    const phrase = '';
-
-    // act ...
-    let result = isPalindrome(phrase);
-
-    // assert ...
-    expect(result).to.equal(false);
-  });
-
-  it(' \'   \' === false', () => {
-    // arrange ...
-    const phrase = '   ';
-
-    // act ...
-    let result = isPalindrome(phrase);
-
-    // assert ...
-    expect(result).to.equal(false);
-  });
-
-  it(' 0 === false', () => {
-    // arrange ...
-    const phrase = 0;
-
-    // act ...
-    let result = isPalindrome(phrase);
-
-    // assert ...
-    expect(result).to.equal(false);
-  });
-
-  it('pass no arguments then throw exception: "Invalid Argument"', () => {
-    // arrange ...
-    const func = function () {isPalindrome();};
-
-    // act ...assert ...
+  it('throw exception when no arguments passed', () => {
+    let func = function () {
+      isPalindrome();
+    };
     expect(func).to.throw(Error, 'Invalid Argument');
   });
 });
