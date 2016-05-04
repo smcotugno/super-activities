849f9a97ca07a6baf62a870baa4a463efb91c920
sfgarage <jdoefer@us.ibm.com>
2016-05-04 13:42:18 -0700

    experimental change

--- client/app/spike/06-prime-factors-test/prime-factors.spec.js
+++ client/app/spike/06-prime-factors-test/prime-factors.spec.js
@@ -4,4 +4,8 @@ describe('prime factors of', () => {
   it('prove the test infrastructure works', () => {
     expect(true).to.equal(true);
   });
+
+  it('the pipeline works', () => {
+    expect(true).to.equal(true);
+  });
 });
