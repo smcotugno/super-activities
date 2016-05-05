ce75bbb9f7933e79b59682d4d278752a33dca1b0
Dave Bellagio <dbellagio@us.ibm.com>
2016-05-04 22:30:24 -0700

    Update tests to match test list

--- client/app/components/activity/activity.spec.js
+++ client/app/components/activity/activity.spec.js
@@ -24,7 +24,7 @@ describe('activity page', () => {
     $scope.$digest();
   }));
 
-  describe('should have', () => {
+  describe('the Activity page should have', () => {
     it('a url called /activity', () => {
       $state.go('activity');
       $scope.$apply();
@@ -38,8 +38,52 @@ describe('activity page', () => {
 
     it('indicator top right for current activity', () => {
       // console.log($(element).find('h1'));
-      expect($(element).find('h1').text()).to.equal('hello activity');
+      expect($(element).find('h1').text()).to.equal('current activity');
     });
 
+    it('list of canned activities', () => {
+        // console.log($(element).find('h1'));
+        expect($(element).find('h1').text()).to.equal('activity list');
+      });
+
+    it('current activity is greyed out', () => {
+        // console.log($(element).find('h1'));
+        expect($(element).find('h1').text()).to.equal('current activity');
+      });
+  });
+
+  describe('on the page, the top right indicator should have', () => {
+	  it('current activity', () => {
+		  $state.go('activity');
+		  // select activity
+		  $scope.$apply();
+		  expect($(element).find('h1').text()).to.equal('current activity');
+	  });
+
+	  it('clicking current activity should bring you to activity details', () => {
+		  $state.go('current activity');
+		  // console.log($(element).find('h1'));
+		  $scope.$apply();
+		  expect($(element).find('h1').text()).to.equal('activity name');
+	  });	    
+  });
+  
+  describe('on the page, list of canned activities should have', () => {
+	  it('activity name', () => {
+		  $state.go('activity');
+		  $scope.$apply();
+		  expect($(element).find('h1').text()).to.equal('activity name');
+	  });
+	  
+	  it('point value', () => {
+		  expect($(element).find('h1').text()).to.equal('point value');
+	  });	
+	  
+	  it('clicking activity in list will bring you to activity details', () => {
+		  $state.go('activity name');	    	  
+		  // console.log($(element).find('h1'));
+		  $scope.$apply();
+		  expect($(element).find('h1').text()).to.equal('activity name');
+	  });  	    
   });
 });
