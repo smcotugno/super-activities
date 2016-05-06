48d83099edc426be8b2b6797c47982fc5ade2236
sfgarage <jdoefer@us.ibm.com>
2016-05-06 14:00:51 -0700

    update details controller

--- client/app/components/details/details.controller.js
+++ client/app/components/details/details.controller.js
@@ -2,39 +2,31 @@ let DetailsController = ($scope, $state) => {
   let currentActivity = function (activity) {
     return {
       name: activity.name,
-      details: activity.details,
+      description: activity.description,
       points: activity.points,
       isActive: false
     };
   };
 
-  //TODO: Replace these with the proper references, if we come up with those
   $scope.currentActivity = currentActivity({
     name: 'Basketball',
-    details: 'Ball, Hoop, Court',
+    description: 'Ball, Hoop, Court',
     points: 40
   });
 
-  //$scope.isCurrent = false;
   $scope.start = function start() {
-      $scope.currentActivity.isCurrent = true;
+      $scope.currentActivity.isActive = true;
   };
   $scope.cancel = function cancel() {
-    $scope.currentActivity.isCurrent = false;
+    $scope.currentActivity.isActive = false;
     $state.go('home');
   };
 
   $scope.done = function done() {
-    $scope.currentActivity.isCurrent = false;
+    $scope.currentActivity.isActive = false;
     $state.go('points');
   };
 };
 DetailsController.$inject = ['$scope', '$state'];
 
 export {DetailsController};
-
-
-//let $scope.someActivity =
-//{
- // inProgress: false
-//};
