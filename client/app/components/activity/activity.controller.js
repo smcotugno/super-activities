b431e274c085c9be9148fac0ae1fdb9ef3c3cf8a
Dave Bellagio <dbellagio@us.ibm.com>
2016-06-01 09:55:57 -0700

    Add in the hockey activity

--- client/app/components/activity/activity.controller.js
+++ client/app/components/activity/activity.controller.js
@@ -1,49 +1,57 @@
 let ActivityController = ($scope, $state, theCurrentActivity) => {
 
-$scope.myActivity = theCurrentActivity;
+	$scope.myActivity = theCurrentActivity;
 
-$scope.sampleActivity1 = {
-    name: 'Basketball',
-    description: 'This is a description of Basketball',
-    points: 50
-  };
+	$scope.sampleActivity1 = {
+	    name: 'Basketball',
+	    description: 'This is a description of Basketball',
+	    points: 50
+	  };
 
-$scope.sampleActivity2 = {
-    name: 'Baseball',
-    description: 'This is a description of Baseball',
-    points: 40
-  };
+	$scope.sampleActivity2 = {
+	    name: 'Baseball',
+	    description: 'This is a description of Baseball',
+	    points: 40
+	  };
 
-$scope.sampleActivity3 = {
-    name: 'Football',
-    description: 'This is a description of Football',
-    points: 60
-  };
+	$scope.sampleActivity3 = {
+	    name: 'Football',
+	    description: 'This is a description of Football',
+	    points: 60
+	  };
 
-$scope.goToActivityDetails = function goToActivityDetails(parameter1) {
+	$scope.sampleActivity4 = {
+	    name: 'Hockey',
+	    description: 'This is a description of Hockey',
+	    points: 30
+	  };
+	
+	$scope.goToActivityDetails = function goToActivityDetails(parameter1) {
      // populate object
- //   $scope.myActivity.set(num);
-    if ( parameter1 === '1' ) {
-	  $scope.myActivity.set($scope.sampleActivity1);
-    } else if ( parameter1 === '2' ) {
-      $scope.myActivity.set($scope.sampleActivity2);
-    } else {
-      $scope.myActivity.set($scope.sampleActivity3);    	
-    }
-    $state.go('details');
-  };
+	 //   $scope.myActivity.set(num);
+	    if ( parameter1 === '1' ) {
+		  $scope.myActivity.set($scope.sampleActivity1);
+	    } else if ( parameter1 === '2' ) {
+	      $scope.myActivity.set($scope.sampleActivity2);
+	    } else if ( parameter1 === '3' ) {
+	      $scope.myActivity.set($scope.sampleActivity3);
+	    } else {
+	      $scope.myActivity.set($scope.sampleActivity4);    	
+	    }
+	    $state.go('details');
+   };
 
-$scope.goToPoints = function goToPoints() {
-    $state.go('points');
-  };
+	$scope.goToPoints = function goToPoints() {
+	    $state.go('points');
+	  };
 
-$scope.goToHome = function goToHome() {
-    $state.go('home');
-  };
+	$scope.goToHome = function goToHome() {
+	    $state.go('home');
+	  };
 
-  //$http.get("http://localhost:3000/activities").then(function (response) {
-  //  $scope.myData = response.data;
-  //});
+	  //$http.get("http://localhost:3000/activities").then(function (response) {
+	  //  $scope.myData = response.data;
+	  //});
 };
 
 ActivityController.$inject = ['$scope', '$state', 'theCurrentActivity'];
