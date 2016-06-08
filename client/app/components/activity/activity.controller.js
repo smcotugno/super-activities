12ff9ffb752282d829c0e9d25fffbe4a9621cea2
Dave Bellagio <dbellagio@us.ibm.com>
2016-06-07 23:31:11 -0700

    Change the activity controller to display activities from a Cloudants data base

--- client/app/components/activity/activity.controller.js
+++ client/app/components/activity/activity.controller.js
@@ -2,8 +2,92 @@ var dbCredentials = {
 	dbName : 'super-activities-db'
 };
 
+var lastCredsUsed = '';
+
 // var db;
 // var cloudant;
+
+function encodeBase64(stringToEncode) {
+	
+    const keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+  
+    var output = "";
+    var chr1, chr2, chr3 = "";
+    var enc1, enc2, enc3, enc4 = "";
+    var i = 0;
+  
+    do {
+        chr1 = stringToEncode.charCodeAt(i++);
+        chr2 = stringToEncode.charCodeAt(i++);
+        chr3 = stringToEncode.charCodeAt(i++);
+  
+        enc1 = chr1 >> 2;
+        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+        enc4 = chr3 & 63;
+  
+        if (isNaN(chr2)) {
+           enc3 = enc4 = 64;
+        } else if (isNaN(chr3)) {
+           enc4 = 64;
+        }
+  
+        output = output +
+                keyStr.charAt(enc1) +
+                keyStr.charAt(enc2) +
+                keyStr.charAt(enc3) +
+                keyStr.charAt(enc4);
+        chr1 = chr2 = chr3 = "";
+        enc1 = enc2 = enc3 = enc4 = "";
+    } while (i < stringToEncode.length);
+  
+    return output;
+}
+
+
+ function base64Decode(stringToDecode) {
+ 
+      var output = "";
+      var chr1, chr2, chr3 = "";
+      var enc1, enc2, enc3, enc4 = "";
+      var i = 0;
+  
+      // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+      var base64test = /[^A-Za-z0-9\+\/\=]/g;
+      if (base64test.exec(stringToDecode)) {
+          console.warn("There were invalid base64 characters in the input text.\n" +
+                    "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                    "Expect errors in decoding.");
+      }
+      stringToDecode = stringToDecode.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+  
+      do {
+          enc1 = keyStr.indexOf(stringToDecode.charAt(i++));
+          enc2 = keyStr.indexOf(stringToDecode.charAt(i++));
+          enc3 = keyStr.indexOf(stringToDecode.charAt(i++));
+          enc4 = keyStr.indexOf(stringToDecode.charAt(i++));
+  
+          chr1 = (enc1 << 2) | (enc2 >> 4);
+          chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+          chr3 = ((enc3 & 3) << 6) | enc4;
+  
+          output = output + String.fromCharCode(chr1);
+  
+          if (enc3 != 64) {
+               output = output + String.fromCharCode(chr2);
+          }
+          if (enc4 != 64) {
+               output = output + String.fromCharCode(chr3);
+          }
+  
+          chr1 = chr2 = chr3 = "";
+          enc1 = enc2 = enc3 = enc4 = "";
+  
+      } while (i < stringToDecode.length);
+  
+      return output;
+}
+ 
 
 function initDBConnection() {
 	
@@ -38,7 +122,7 @@ function initDBConnection() {
 			console.warn('Could not find Cloudant credentials in VCAP_SERVICES environment variable - data will be unavailable to the UI');
 
 		}
-	} else{
+	} else {
 		console.warn('VCAP_SERVICES environment variable not set - data will be unavailable to the UI');
 		// For running this app locally you can get your Cloudant credentials 
 		// from Bluemix (VCAP_SERVICES in "cf env" output or the Environment 
@@ -53,14 +137,14 @@ function initDBConnection() {
 		dbCredentials.password = "39ed55d43de58e4027705af1b65c1f30e5039e3a41a930655f3498ec382a6586";
 		dbCredentials.url = "https://ed939949-f653-4827-80d7-b337bff5fdbc-bluemix:39ed55d43de58e4027705af1b65c1f30e5039e3a41a930655f3498ec382a6586@ed939949-f653-4827-80d7-b337bff5fdbc-bluemix.cloudant.com";
 	}
+		
 }
 
 let ActivityController = ($scope, $state, $http, theCurrentActivity) => {
 
 	$scope.myActivity = theCurrentActivity;
-	
-	initDBConnection();
 
+	/*
 	$scope.sampleActivity1 = {
 	    name: 'Basketball',
 	    description: 'This is a description of Basketball',
@@ -115,6 +199,14 @@ let ActivityController = ($scope, $state, $http, theCurrentActivity) => {
 	    }
 	    $state.go('details');
    };
+   
+   */
+   
+   $scope.goToDBActivityDetails = function goToDBActivityDetails(activityData) {
+	     // populate object
+		 $scope.myActivity.set(activityData);
+		 $state.go('details');
+	   };
 
 	$scope.goToPoints = function goToPoints() {
 	    $state.go('points');
@@ -124,10 +216,42 @@ let ActivityController = ($scope, $state, $http, theCurrentActivity) => {
 	    $state.go('home');
 	  };
 
+	initDBConnection();
+
 	var myUrl = dbCredentials.url + "/" + dbCredentials.dbName + "/_all_docs/?include_docs=true";
 	console.log('myURL = ', myUrl);
+	var encodedCredsToUse = '';
+	if ( lastCredsUsed.length > 0 ) {
+		// use the last encoded version
+		encodedCredsToUse = lastCredsUsed;
+	} else {
+		// generate Base64 encoded version
+		encodedCredsToUse = encodeBase64(dbCredentials.user + ":" + dbCredentials.password);
+		lastCredsUsed = encodedCredsToUse;
+	}
+	
+	$http.defaults.headers.common.Authorization = 'Basic ';
+	$http.defaults.headers.common['Authorization'] = 'Basic ' + lastCredsUsed;	
 	$http.get(myUrl).then(function (response) {
-	   $scope.myData = response.data;
+		// GET was OK
+		var activityList = [];
+		console.log("Total rows = " + response.data.total_rows);
+		if ( response.data.total_rows > 0 ) {
+	       // build up the structure of data like we need it, should be a list of Activity objects
+		   var i = 0;
+		   do {
+			   activityList[i] = {};
+			   activityList[i].name = response.data.rows[i].id;
+			   activityList[i].description = response.data.rows[i].doc.description;
+			   activityList[i].points = response.data.rows[i].doc.points;
+			   i++;
+		   } while ( i < response.data.total_rows );
+	   }   
+	   $scope.myData = activityList;
+	}, function (response) {
+	    //Second function handles error
+		var errorMes = "GET error = " + response.status + ": " + response.statusText;
+		console.warn(errorMes);
 	});
 	
 };
